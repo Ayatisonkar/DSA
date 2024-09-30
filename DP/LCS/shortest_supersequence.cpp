597eaa5fcfea5546e5ce72bbc76fc32d4e42ef77
Ayatisonkar <97397251+Ayatisonkar@users.noreply.github.com>
2024-09-30 17:52:48 +0530

    Create shortest_supersequence.cpp

---
+++ DP/LCS/shortest_supersequence.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h> 
+string shortestSupersequence(string a, string b)
+{
+	string s1=a;
+	string s2=b;
+	int n = s1.size();
+    int m = s2.size();
+    // Create a 2D DP table with dimensions (n+1) x (m+1)
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    
+
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
+            else
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+        }
+    }
+
+    //int len= (n+m)-dp[n][m];
+	 int len = dp[n][m];
+  int i = n;
+  int j = m;
+
+  int index = len - 1;
+  string ans = "";
+
+  while (i > 0 && j > 0) {
+    if (s1[i - 1] == s2[j - 1]) {
+      ans += s1[i-1];
+      index--;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+        ans += s1[i-1];
+        i--;
+    } else {
+        ans += s2[j-1];
+        j--;
+    }
+  }
+  
+  //Adding Remaing Characters - Only one of the below two while loops will run 
+  
+  while(i>0){
+      ans += s1[i-1];
+      i--;
+  }
+  while(j>0){
+      ans += s2[j-1];
+      j--;
+  }
+
+  reverse(ans.begin(),ans.end());
+  
+  return ans;
+}
